7f923220fccce0c7d588f945603d2f290f063d49
gabebonham <grotegabriel@gmail.com>
2024-12-30 21:47:15 -0300

    major adjustments

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/home',
+				permanent: true,
+			},
+		];
+	},
 };
 export default nextConfig;
